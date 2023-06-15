743e0adb6126c681b0dd68f5748cc62b3032d09a
Usman Khan <khanindmgs@gmail.com>
2023-06-16 00:21:06 +0300

    billcalculation

---
+++ index.js
@@ -0,0 +1,18 @@
+let billTotalInput = document.getElementById('billtotal')
+let tipInput = document.getElementById('tipInput')
+let numberOfPeople = document.getElementById('noofpeople')
+let perPersonTotal = document.getElementById('perpersontotal')
+let numpeople = numberOfPeople.innerText
+//adding
+//subing
+const calculateBill = () => {
+  let bill = Number(billTotalInput.value)
+  //console.log(bill)
+  let tip = Number(tipInput.value)/100
+  //console.log(tip)
+  let result=((bill*tip) + bill)/numpeople
+  
+
+  perPersonTotal.innerText = `$${result.toLocaleString('en-US')}`
+}
+billTotalInput.onkeyup = () => calculateBill()
